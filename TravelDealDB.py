b79fc234b8d667ffe20b623f7f9aed48a9eafee2
Daniel Curtis <Daniel.Curtis@ipreo.com>
2019-04-11 15:48:04 -0400

    Add Delete function
    - use table name environment variables in python
    - add env var for index
    - added del method
    - addded get tweets message
    - still need to push stack and test

--- TravelDealDB.py
+++ TravelDealDB.py
@@ -1,9 +1,10 @@
 import boto3
-
+from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 #dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
-twitter_flight_user_table = dynamodb.Table('twitter-flight-user')
-twitter_flight_tweets_table = dynamodb.Table('twitter-flight-tweets')
+twitter_flight_user_table = dynamodb.Table(os.environ['userTableName'])
+twitter_flight_tweets_table = dynamodb.Table(os.environ['tweetsTableName'])
+twitter_flight_tweets_table_created_date_index = os.environ['tweetsTableCreatedDateIndex']
 
 # look into batch insert
 def insert_tweets(tweets):
@@ -35,4 +36,26 @@ def get_user_last_tweet_id(twitter_username):
     else:
         return None
 
-# get tweets
+def get_tweets_by_search_criteria(search_criteria):
+     tweets = twitter_flight_tweets_table.query(
+         KeyConditionExpression=Key('search_terms').contains(search_criteria)
+    )
+    if 'Item' in response:
+        return response['Items']
+    else:
+        return []
+
+def delete_tweets_older_than(older_than):
+    # looks like delete with conditional is not supported so we have to fetch the items then batch them
+    tweets = twitter_flight_tweets_table.query(
+        IndexName = twitter_flight_tweets_table_created_date_index
+        ExpressionAttributeValues={
+            'v1':{
+                'S':''
+            }
+        },
+        KeyConditionExpresson='created_at < :v1'
+    )
+    with twitter_flight_tweets_table.batch_writer() as batch:
+        for tweet in tweets.Items:
+            batch.delete_item(Item=tweet)
