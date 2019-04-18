4fc065287d6c9ad2537a9854683f2c2ce23d9cca
Daniel Curtis <Daniel.Curtis@ipreo.com>
2019-04-18 17:25:07 -0400

    added try except

--- backend/TravelAPI.py
+++ backend/TravelAPI.py
@@ -22,16 +22,20 @@ def convert_tweet_id(tweet):
     return tweet
 
 def get_airport_tweets(event, context):
-    body = json.loads(event['body'])
+    try:
+        body = json.loads(event['body'])
+    except JSONDecodeError:
+        print("JSONDecodeError will return latest results")
+        body = {}
 
     if 'airport' in body:
         response = tddb.get_tweets_by_search_criteria(body['airport'])
     else:
         response = tddb.get_any_tweet()
-    
+
     output = list(map(convert_tweet_id, response))
 
     return {
         "statusCode": 200,
         "body": json.dumps(output)
-    }         
+    }
